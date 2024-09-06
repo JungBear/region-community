92240a2e25c1c8369d764f3767dcd3f9bcce81cd
정상필의GRAM\mod45 <mod459@naver.com>
2024-09-06 15:11:05 +0900

    테스트용 mysql 로컬 버전 세팅

--- backend/src/test/java/com/project/community/CommunityApplicationTests.java
+++ backend/src/test/java/com/project/community/CommunityApplicationTests.java
@@ -1,17 +1,24 @@
 package com.project.community;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class CommunityApplicationTests {
 
+	 @Autowired
+    private ApplicationContext applicationContext;
+
 	@Test
 	void contextLoads() {
 		
-		assert(true);
+		assertNotNull(applicationContext);
 	}
 
 }
