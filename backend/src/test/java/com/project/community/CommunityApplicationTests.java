fbd60591f9f4125c37a97c126a1707b5d0dd1602
정상필의GRAM\mod45 <mod459@naver.com>
2024-09-06 13:01:40 +0900

    test에 문제가 생겨서 stacktrace 보기위한 명령 추가

--- backend/src/test/java/com/project/community/CommunityApplicationTests.java
+++ backend/src/test/java/com/project/community/CommunityApplicationTests.java
@@ -2,12 +2,15 @@ package com.project.community;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CommunityApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assert(true);
 	}
 
 }
