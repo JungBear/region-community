31d21de8d34076e3acd940300ee58f0c1bcbd910
정상필의GRAM\mod45 <mod459@naver.com>
2024-09-06 16:24:01 +0900

    .gradlew bootRun으로 변경

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -2,7 +2,7 @@
 export MYSQL_ID=$(aws ssm get-parameter --name /region-community/MYSQL_ID --with-decryption --query Parameter.Value --output text --region ap-northeast-2)
 export MYSQL_PWD=$(aws ssm get-parameter --name /region-community/MYSQL_PWD --with-decryption --query Parameter.Value --output text --region ap-northeast-2)
 cd /home/ec2-user/app/backend
-nohup java -jar *.jar > /dev/null 2>&1 &
+nohup ./gradlew bootRun > /dev/null 2>&1 &
 
 cd /home/ec2-user/app/frontend
 nohup npx serve -s build -l 3000 > /dev/null 2>&1 &
