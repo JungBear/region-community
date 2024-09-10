70528d0caec5e4dc3c95ac3e385a6c9e1ada343e
정상필의GRAM\mod45 <mod459@naver.com>
2024-09-10 13:39:04 +0900

    test 수정

--- backend/src/test/java/com/project/community/CommunityApplicationTests.java
+++ backend/src/test/java/com/project/community/CommunityApplicationTests.java
@@ -7,11 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 import javax.sql.DataSource;
 
 @SpringBootTest
-@ActiveProfiles("test")
+@TestPropertySource(locations = "classpath:application-test.yml")
 class CommunityApplicationTests {
 
 	 @Autowired
